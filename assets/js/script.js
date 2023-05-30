79b4687bdc3b211ef795521bd8ec2edf91f6d860
Mattias Lundkvist <mindfalls@hotmail.com>
2023-05-30 17:48:05 +0000

    Add lotr Questions to javaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,55 @@
+const  lotr = [
+    {
+        question: "Frodo is Bilbo's?",
+        answers: [
+            {text: "Son",correct: false},
+            {text: "Nephew",correct: true},
+            {text: "Grandchild",correct: false},
+            {text: "Cousin",correct: false},
+        ]
+    },
+    {
+        question: "Where does Frodo Live?",
+        answers: [
+            {text: "Bag End",correct: true},
+            {text: "UnderHill",correct: false},
+            {text: "Baggins",correct: false},
+            {text: "Needlehole",correct: false},
+        ]
+    },
+    {
+        question: "How many rings for the Dwarves?",
+        answers: [
+            {text: "3",correct: true},
+            {text: "7",correct: false},
+            {text: "9",correct: false},
+            {text: "4",correct: false},
+        ]
+    },
+    {
+        question: "Who leads the wizard order?",
+        answers: [
+            {text: "Gandalf",correct: false},
+            {text: "Saruman",correct: true},
+            {text: "Radagast",correct: false},
+            {text: "Elrond",correct: false},
+        ]
+    },
+    {
+        question: "Who carries Frodo to Rivendell?",
+        answers: [
+            {text: "Gandalf",correct: true},
+            {text: "Arwen",correct: false},
+            {text: "Glorfindel",correct: false},
+            {text: "Aragorn",correct: false},
+        ]
+    },
+    
+
+]
+
+
+
 function startGame() {
 
 }
